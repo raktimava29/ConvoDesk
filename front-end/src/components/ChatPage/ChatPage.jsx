5bcb31de4f3aa923356b5154018e832602ed3645
raktimava29 <raktim.bhatta29@gmail.com>
2024-10-11 02:48:04 +0530

    Add Chats

--- front-end/src/components/ChatPage/ChatPage.jsx
+++ front-end/src/components/ChatPage/ChatPage.jsx
@@ -5,19 +5,24 @@ import MyChats from "./MyChats";
 import SideDrawer from "../Extras/SideDrawer";
 import { ChatState } from "./ChatProvider";
 
-export default function Chatpage(){
+export default function Chatpage() {
   const [fetchAgain, setFetchAgain] = useState(false);
   const { user } = ChatState();
 
   return (
-    <div className="w-full">
+    <>
       {user && <SideDrawer />}
-      {/* <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        height={{ base:"92vh" , sm:"90vh"}}
+        padding="10px"
+      >
         {user && <MyChats fetchAgain={fetchAgain} />}
         {user && (
           <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
         )}
-      </Box> */}
-    </div>
+      </Box>
+    </>
   );
 }
