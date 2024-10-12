d47b5606f29452d1ff9a4695ce8362d8a2310bec
raktimava29 <raktim.bhatta29@gmail.com>
2024-10-12 17:43:28 +0530

    Group Chat Modal

---
+++ back-end/routers/dist/user-controller.dev.js
@@ -0,0 +1,171 @@
+"use strict";
+
+var asyncHandler = require("express-async-handler");
+
+var User = require("../models/userModel");
+
+var genToken = require("../config/genToken");
+
+var registerUser = asyncHandler(function _callee(req, res) {
+  var _req$body, name, email, password, userExists, user;
+
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _req$body = req.body, name = _req$body.name, email = _req$body.email, password = _req$body.password;
+
+          if (!(!name || !email || !password)) {
+            _context.next = 4;
+            break;
+          }
+
+          res.status(400);
+          throw new Error("Cannot Register User");
+
+        case 4:
+          _context.next = 6;
+          return regeneratorRuntime.awrap(User.findOne({
+            email: email
+          }));
+
+        case 6:
+          userExists = _context.sent;
+
+          if (!userExists) {
+            _context.next = 10;
+            break;
+          }
+
+          res.status(400);
+          throw new Error("User already exists");
+
+        case 10:
+          _context.next = 12;
+          return regeneratorRuntime.awrap(User.create({
+            name: name,
+            email: email,
+            password: password
+          }));
+
+        case 12:
+          user = _context.sent;
+
+          if (!user) {
+            _context.next = 17;
+            break;
+          }
+
+          res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: genToken(user._id)
+          });
+          _context.next = 19;
+          break;
+
+        case 17:
+          res.status(400);
+          throw new Error("User not found");
+
+        case 19:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+});
+var authUser = asyncHandler(function _callee2(req, res) {
+  var _req$body2, email, password, user;
+
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _req$body2 = req.body, email = _req$body2.email, password = _req$body2.password;
+          _context2.next = 3;
+          return regeneratorRuntime.awrap(User.findOne({
+            email: email
+          }));
+
+        case 3:
+          user = _context2.sent;
+          _context2.t0 = user;
+
+          if (!_context2.t0) {
+            _context2.next = 9;
+            break;
+          }
+
+          _context2.next = 8;
+          return regeneratorRuntime.awrap(user.matchPassword(password));
+
+        case 8:
+          _context2.t0 = _context2.sent;
+
+        case 9:
+          if (!_context2.t0) {
+            _context2.next = 13;
+            break;
+          }
+
+          res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: genToken(user._id)
+          });
+          _context2.next = 15;
+          break;
+
+        case 13:
+          res.status(401);
+          throw new Error("Invalid Email or Password");
+
+        case 15:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+});
+var allUsers = asyncHandler(function _callee3(req, res) {
+  var keyword, users;
+  return regeneratorRuntime.async(function _callee3$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          keyword = req.query.search ? {
+            $or: [{
+              name: {
+                $regex: req.query.search,
+                $options: "i"
+              }
+            }]
+          } : {};
+          _context3.next = 3;
+          return regeneratorRuntime.awrap(User.find(keyword).find({
+            _id: {
+              $ne: req.user._id
+            }
+          }));
+
+        case 3:
+          users = _context3.sent;
+          res.send(users);
+
+        case 5:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  });
+});
+module.exports = {
+  registerUser: registerUser,
+  authUser: authUser,
+  allUsers: allUsers
+};
---
+++ front-end/src/components/ChatPage/GroupChatModal.jsx
@@ -0,0 +1,175 @@
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react";
+import { Button, useDisclosure, FormControl, Input, useToast, Box } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import { ChatState } from "./ChatProvider";
+// import UserBadgeItem from "../userAvatar/UserBadgeItem";
+import UserListItem from "../Extras/UserListItem"
+
+export default function GroupChatModal({ children }){
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState("");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  const { user, chats, setChats } = ChatState();
+
+  const handleGroup = (userToAdd) => {
+    if (selectedUsers.includes(userToAdd)) {
+      toast({
+        title: "User already added",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      return;
+    }
+
+    setSelectedUsers([...selectedUsers, userToAdd]);
+  };
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if(!query.trim()){
+      setSearchResult([]); 
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${query}`, config);
+      setSearchResult(data);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occurred!",
+        description: "Failed to load the search results.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = (delUser) => {
+    setSelectedUsers(selectedUsers.filter((sel) => sel._id !== delUser._id));
+  };
+
+  const handleSubmit = async () => {
+    if (!groupChatName || !selectedUsers.length) {
+      toast({
+        title: "Please fill all the fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post(
+        `/api/chat/group`,
+        {
+          name: groupChatName,
+          users: JSON.stringify(selectedUsers.map((u) => u._id)),
+        },
+        config
+      );
+      setChats([data, ...chats]);
+      onClose();
+      toast({
+        title: "New Group Chat Created!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    } catch (error) {
+      toast({
+        title: "Failed to Create the Chat!",
+        description: error.response?.data || "Something went wrong.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+
+  return (
+    <>
+      <span onClick={onOpen}>{children}</span>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="suse"
+            display="flex"
+            justifyContent="center"
+          >
+            Create Group Chat
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display="flex" flexDirection="column" alignItems="center">
+            <FormControl mb={3}>
+              <Input
+                placeholder="Chat Name"
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+            </FormControl>
+            <FormControl mb={1}>
+              <Input
+                placeholder="Add Users"
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {/* <Box w="100%" display="flex" flexWrap="wrap">
+              {selectedUsers.map((u) => (
+                <UserBadgeItem
+                  key={u._id}
+                  user={u}
+                  handleFunction={() => handleDelete(u)}
+                />
+              ))}
+            </Box> */}
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              searchResult?.slice(0, 4).map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => handleGroup(user)}
+                />
+              ))
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleSubmit} colorScheme="blue">
+              Create Chat
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
